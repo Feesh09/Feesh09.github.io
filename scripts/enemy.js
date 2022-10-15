9f4217bdf76c2a5a192f05836fca0f1d07761e53
Sergeant Fuzzyboots <91094449+Feesh09@users.noreply.github.com>
2022-10-15 15:10:01 -0500

    Update enemy.js

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -21,8 +21,7 @@ class Enemy {
         this.immune = [];           // no damage from these damage types
         this.resistant = [];        // reduced damage from these damage types
         this.weak = [];             // increased damage from these damage types
-        // SPEED
-        this.speed = 4;             // 4 is the max
+        this.speed = 1;             // 4 is the max
         this.taunt = false;         // force towers to target
     }
 
